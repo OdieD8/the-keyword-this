4207a5356fe0de3f94344441a7426a34b7da3921
OdieD8 <odie_de_moura@hotmail.com>
2015-10-20 16:37:09 -0600

    Second commit

--- practice.js
+++ practice.js
@@ -2,14 +2,18 @@
   // 1) What is the purpose of the 'this keyword'?
 
       //Answer
+      The this keyword is a shortcut that allows you to write less code by not having to keep referring to objects.
 
   // 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
 
       //Answer
-
+      New binding, implicit binding, explicit binding, window binding
+      
   // 3) What is the difference between call and apply?
 
       //Answer
+call references the "this" keyword
+apply sets the "this" keyword
 
   // 4) What does .bind do?
 
@@ -21,12 +25,19 @@
 //Create an object called user which has the following properties.
   //username --> which is a string
   //email --> which is a string
-  //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
+  //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 
+  //'this' keyword*
 
     //Code Here
-
+var user = {
+  username: 'Odiie',
+  email: 'e-mail address',
+  getUsername: function() {
+   return this.username;
+  }
+};
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
-
+console.log(user.getUsername());
 
 //Next Problem
 
@@ -34,6 +45,17 @@
 // Write the function definitions which will make the following function invocations function properly.
 
   //Function Invocations Here
+  
+function Car(make, model, year) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.move = 0;
+  this.moveCar = function() {
+    this.move += 10;
+    return this.move;
+  };
+}
 
 var prius = new Car('Toyota', 'Prius', 2011);
 var mustang = new Car('Ford', 'Mustang', 2013);
@@ -41,8 +63,11 @@ var mustang = new Car('Ford', 'Mustang', 2013);
 prius.moveCar(); //increments prius' move property by 10. Returns the new move property.
 mustang.moveCar(); //increments mustang' move property by 10. Returns the new move property.
 
-//Hint, you'll need to write a moveCar function which is added to every object that is being returned from the Car function. You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the write object (prius vs mustang).
+//Hint, you'll need to write a moveCar function which is added to every object that is being returned from the Car function. 
+//You'll also need to use the 'this' keyword properly in order to make sure you're invoking moveCar on the write object 
+//(prius vs mustang).
 
+ 
 
 
 //Continuation of previous problem
@@ -51,10 +76,13 @@ var getYear = function(){
   return this.year;
 };
 
-//Above you're given the getYear function. Using your prius and mustang objects from above, use the proper syntax that will allow for you to call the getYear function with the prius then the mustang objects being the focal objects. *Don't add getYear as a property on both objects*.
+//Above you're given the getYear function. Using your prius and mustang objects from above, use the proper syntax that will allow 
+//for you to call the getYear function with the prius then the mustang objects being the focal objects. *Don't add getYear as a 
+//property on both objects*.
 
   //Code Here
-
+console.log(getYear.call(prius));
+console.log(getYear.call(mustang));
 
 
 //New Problem
@@ -71,14 +99,18 @@ var getUsername = function(){
   console.log(this.username);
 };
 
-setTimeout(getUsername, 5000);
-
-//Above you're given an object, a function, and a setTimeout invocation. After 5 seconds, what will the getUsername function return?
+setTimeout(getUsername.bind(user), 5000);
+//Above you're given an object, a function, and a setTimeout invocation. After 5 seconds, what will the getUsername function 
+//return?
 
   //Answer Here
+  It will return undefined.
 
 //In the example above, what is the 'this keyword' bound to when getUsername runs?
 
   //Answer Here
+  It\s bound to getUsername.
+  
 
 //Fix the setTimeout invocation so that the user object will be the focal object when getUsername is ran.
+setTimeout(getUsername.bind(user), 5000);
